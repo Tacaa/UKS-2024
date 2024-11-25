3eda521255d96a7cd7d86d85a4a1c270dfb35d28
n-vojin <80399345+n-vojin@users.noreply.github.com>
2024-11-25 22:47:36 +0100

    feat: add header for repositories component
    add header for repositories component
    add more global variables
    add temporary services for generating dummy data
    add models (might also be temporary)
    issue #4

--- frontend/uks-ui/src/app/menu-pages/repositories/repositories.component.ts
+++ frontend/uks-ui/src/app/menu-pages/repositories/repositories.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RepositoryService } from 'src/app/services/repository/repository.service';
+import { Repository } from 'src/app/shared/models/Repository';
 
 @Component({
   selector: 'app-repositories',
   templateUrl: './repositories.component.html',
-  styleUrls: ['./repositories.component.css']
+  styleUrls: ['./repositories.component.css'],
 })
-export class RepositoriesComponent {
+export class RepositoriesComponent implements OnInit {
+  repos: Repository[] = [];
+  namespaces: string[] = [];
 
+  constructor(private repositoryService: RepositoryService) {}
+
+  ngOnInit(): void {
+    this.repos = this.repositoryService.getAll();
+    this.namespaces = this.repositoryService.getAllNamespaces();
+  }
 }
---
+++ frontend/uks-ui/src/app/shared/models/Repository.ts
@@ -0,0 +1,12 @@
+export class Repository {
+  id!: number;
+  name!: string;
+  namespace!: string;
+  description?: string;
+  visibility!: boolean;
+  created!: Date;
+  updated?: Date;
+  star: number = 0;
+  presonal?: boolean;
+  public?: boolean;
+}
