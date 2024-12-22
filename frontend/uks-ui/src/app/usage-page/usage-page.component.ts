2f2fbb231d81b79b84fb20052e0a56fdee0add0a
n-vojin <80399345+n-vojin@users.noreply.github.com>
2024-12-22 18:20:50 +0100

    feat: add select option for namespace
    issue #28

--- frontend/uks-ui/src/app/usage-page/usage-page.component.ts
+++ frontend/uks-ui/src/app/usage-page/usage-page.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RepositoryService } from '../services/repository/repository.service';
+import { FormControl } from '@angular/forms';
+
+interface namespaceGroup {
+  name: string;
+  namespace: string[];
+}
 
 @Component({
   selector: 'app-usage-page',
   templateUrl: './usage-page.component.html',
-  styleUrls: ['./usage-page.component.css']
+  styleUrls: ['./usage-page.component.css'],
 })
-export class UsagePageComponent {
+export class UsagePageComponent implements OnInit {
+  formControl = new FormControl('');
+  organizationNamespaces: string[] = [];
+  selected: string = '';
+  namespaceGroups?: namespaceGroup[];
 
+  constructor(private repositoryService: RepositoryService) {}
+
+  onSelectionChange(event: any): void {
+    this.selected = event.value; // 'event.value' contains the selected namespace
+  }
+
+  ngOnInit(): void {
+    this.organizationNamespaces = this.repositoryService.getAllNamespaces();
+    this.namespaceGroups = [
+      {
+        name: 'Personal',
+        namespace: ['userName'],
+      },
+      {
+        name: 'Organizations',
+        namespace: this.repositoryService.getAllNamespaces(),
+      },
+    ];
+  }
 }
