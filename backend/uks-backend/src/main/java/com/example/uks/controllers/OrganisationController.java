f524747bffe92e7e7e9688d04cdc8bb19806f740
Tatjana Gavrilović <tatjana.gavrilovic@openfyle.com>
2025-06-21 22:11:29 +0200

    feat: add organisation overview

---
+++ backend/uks-backend/src/main/java/com/example/uks/controllers/OrganisationController.java
@@ -0,0 +1,42 @@
+package com.example.uks.controllers;
+
+import com.example.uks.dto.organisation.OrganisationDTO;
+import com.example.uks.dto.repository.RepositoryDTO;
+import com.example.uks.model.Organisation;
+import com.example.uks.services.OrganisationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "api/organisation")
+public class OrganisationController {
+
+    @Autowired
+    private OrganisationService organisationService;
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<OrganisationDTO>> getUserOrganisations(@PathVariable Integer userId) {
+        List<Organisation> organisations = organisationService.getUserOrganisations(userId);
+
+        List<OrganisationDTO> organisationDTOs = new ArrayList<>();
+        for (Organisation organisation : organisations) {
+            organisationDTOs.add(OrganisationDTO.from(organisation));
+        }
+
+        return new ResponseEntity<>(organisationDTOs, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrganisationDTO> getOrganisationById(@PathVariable Integer id) {
+        Organisation organisation = organisationService.getOrganisationById(id);
+        return new ResponseEntity<>(OrganisationDTO.from(organisation), HttpStatus.OK);
+    }
+}
--- backend/uks-backend/src/main/java/com/example/uks/repositories/OrganisationRepository.java
+++ backend/uks-backend/src/main/java/com/example/uks/repositories/OrganisationRepository.java
@@ -1,8 +1,16 @@
 package com.example.uks.repositories;
 
 import com.example.uks.model.Organisation;
+import com.example.uks.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrganisationRepository extends JpaRepository<Organisation, Integer> {
+import java.util.List;
+import java.util.Optional;
 
+public interface OrganisationRepository extends JpaRepository<Organisation, Integer> {
+    List<Organisation> findByOwner(User owner);
+
+    List<Organisation> findByMembersContains(User user);
+
+    Optional<Organisation> findById(Integer id);
 }
