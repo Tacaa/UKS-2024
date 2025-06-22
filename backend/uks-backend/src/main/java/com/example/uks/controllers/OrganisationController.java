ff307ddb9a353f364da71124577a0c3b46cce5c0
Tatjana Gavrilović <tatjana.gavrilovic@openfyle.com>
2025-06-22 08:56:10 +0200

    feat: add controller post method for create

--- backend/uks-backend/src/main/java/com/example/uks/controllers/OrganisationController.java
+++ backend/uks-backend/src/main/java/com/example/uks/controllers/OrganisationController.java
@@ -1,5 +1,6 @@
 package com.example.uks.controllers;
 
+import com.example.uks.dto.organisation.OrganisationCreateDTO;
 import com.example.uks.dto.organisation.OrganisationDTO;
 import com.example.uks.dto.repository.RepositoryDTO;
 import com.example.uks.model.Organisation;
@@ -7,10 +8,7 @@ import com.example.uks.services.OrganisationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,7 +20,11 @@ public class OrganisationController {
     @Autowired
     private OrganisationService organisationService;
 
-
+    @PostMapping
+    public ResponseEntity<OrganisationDTO> createOrganisation(@RequestBody OrganisationCreateDTO dto) {
+        Organisation organisation = organisationService.createOrganisation(dto);
+        return new ResponseEntity<>(OrganisationDTO.from(organisation), HttpStatus.CREATED);
+    }
 
 
 }
