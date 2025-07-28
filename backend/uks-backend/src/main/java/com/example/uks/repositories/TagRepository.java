cfbf7ff67efc4c82a0149505dbdd051e05ba4dab
Tatjana Gavrilović <tatjana.gavrilovic@openfyle.com>
2025-07-28 20:14:32 +0200

    feat: add filterin by word in name and sort by pull time

--- backend/uks-backend/src/main/java/com/example/uks/repositories/TagRepository.java
+++ backend/uks-backend/src/main/java/com/example/uks/repositories/TagRepository.java
@@ -2,9 +2,14 @@ package com.example.uks.repositories;
 
 import com.example.uks.model.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface TagRepository extends JpaRepository<Tag, Integer> {
     List<Tag> findAllByRepositoryId(Long repositoryId);
+
+    @Query("SELECT t FROM Tag t WHERE LOWER(t.name) LIKE LOWER(CONCAT('%', :query, '%'))")
+    List<Tag> searchByNameContaining(@Param("query") String query);
 }
--- backend/uks-backend/src/main/java/com/example/uks/services/TagService.java
+++ backend/uks-backend/src/main/java/com/example/uks/services/TagService.java
@@ -9,10 +9,13 @@ import com.example.uks.repositories.TagRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
 
 @Service
 public class TagService {
+
+    private static final List<String> pulledPriority = List.of("1 day", "2 days", "3 days", "5 days", "about a week", "about a month");
 
     @Autowired
     private TagRepository tagRepository;
@@ -37,5 +40,17 @@ public class TagService {
 
     public List<Tag> getAllTagsByRepository(Long repositoryId) {
         return tagRepository.findAllByRepositoryId(repositoryId);
+    }
+
+    public List<Tag> searchAndSortTags(String query) {
+        List<Tag> tags = tagRepository.searchByNameContaining(query);
+
+        tags.sort(Comparator.comparingInt(t -> {
+            String pulled = t.getPulled();
+            int index = pulledPriority.indexOf(pulled);
+            return index != -1 ? index : Integer.MAX_VALUE;
+        }));
+
+        return tags;
     }
 }
