de6426391c0a2b389ecb786ca35609a214ca3f55
n-vojin <80399345+n-vojin@users.noreply.github.com>
2025-07-31 00:29:35 +0200

    feat: add personal starred repos

--- frontend/uks-ui/src/app/user/user-profile/user-profile.component.ts
+++ frontend/uks-ui/src/app/user/user-profile/user-profile.component.ts
@@ -4,6 +4,8 @@ import { Router } from '@angular/router';
 import { Repository } from 'src/app/shared/models/mock.repository.model';
 import { RepositoryService } from 'src/app/services/mock-repository/repository.service';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { StarService } from 'src/app/services/star/star.service';
+import { RepositoryDTO } from 'src/app/shared/dto/repository/repository.dto';
 
 @Component({
   selector: 'app-user-profile',
@@ -11,13 +13,23 @@ import { AuthService } from 'src/app/services/auth/auth.service';
   styleUrls: ['./user-profile.component.css'],
 })
 export class UserProfileComponent implements OnInit {
-  selectedOption: string = 'repositories';
+  selectedOption: string = 'starred';
   name = this.authService.getCurrentUser();
+  starredRepos: RepositoryDTO[] = [];
+  displayedColumns: string[] = ['name', 'lastPushed', 'organisation'];
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private starService: StarService
+  ) {}
 
   ngOnInit(): void {
     const user = this.authService.getCurrentUser();
     const fullName = `${user?.firstName} ${user?.lastName}`;
+    this.starService
+      .getStarredRepositories(this.authService.getCurrentUser()?.id as number)
+      .subscribe((repos) => {
+        this.starredRepos = repos;
+      });
   }
 }
