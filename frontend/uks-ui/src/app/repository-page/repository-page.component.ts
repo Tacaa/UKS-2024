59dc2440f47c4a15d9dc0044421b5a39848e7b58
n-vojin <80399345+n-vojin@users.noreply.github.com>
2025-07-30 22:02:53 +0200

    feat: starring functionality

--- frontend/uks-ui/src/app/repository-page/repository-page.component.ts
+++ frontend/uks-ui/src/app/repository-page/repository-page.component.ts
@@ -3,6 +3,9 @@ import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { RepositoryService } from '../services/repository/repository.service';
 import { RepositoryDTO } from '../shared/dto/repository/repository.dto';
+import { AuthService } from '../services/auth/auth.service';
+import { StarService } from '../services/star/star.service';
+import { StarDTO } from '../shared/dto/star/star.dto';
 
 @Component({
   selector: 'app-repository-page',
@@ -11,18 +14,53 @@ import { RepositoryDTO } from '../shared/dto/repository/repository.dto';
 })
 export class RepositoryPageComponent implements OnInit {
   repoId: number | null = null;
-  repository$?: Observable<RepositoryDTO>;
+  repository?: RepositoryDTO;
+  starredRepoIds: number[] = [];
+  isStarred: boolean = false;
+  currentUserId: number;
 
   constructor(
     private route: ActivatedRoute,
-    private repositoryService: RepositoryService
-  ) {}
+    private repositoryService: RepositoryService,
+    private authService: AuthService,
+    private starService: StarService
+  ) {
+    this.currentUserId = this.authService.getCurrentUser()?.id as number;
+  }
 
   ngOnInit() {
     this.repoId = Number(this.route.snapshot.paramMap.get('id'));
-
     if (!this.repoId) return;
 
-    this.repository$ = this.repositoryService.getRepositoryById(this.repoId);
+    this.repositoryService.getRepositoryById(this.repoId).subscribe((repo) => {
+      this.repository = repo;
+    });
+
+    this.starService
+      .getStarredRepositories(this.currentUserId)
+      .subscribe((repos) => {
+        this.starredRepoIds = repos.map((r) => r.id as number);
+        this.isStarred = this.starredRepoIds.includes(this.repoId as number);
+      });
+  }
+
+  starClick() {
+    if (!this.repoId || !this.repository) return;
+
+    if (this.isStarred) {
+      this.starService.unstarRepository(this.repoId).subscribe(() => {
+        this.isStarred = false;
+        this.repository!.star -= 1;
+      });
+    } else {
+      const starDTO: StarDTO = {
+        userId: this.currentUserId,
+        repositoryId: this.repoId,
+      };
+      this.starService.starRepository(this.repoId, starDTO).subscribe(() => {
+        this.isStarred = true;
+        this.repository!.star += 1;
+      });
+    }
   }
 }
